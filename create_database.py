793094ccc3ed9475a7b8ab5f1e3eb6615cca86d4
Mal Nushi <malnushi1999+dev@gmail.com>
2024-04-03 18:48:42 -0400

    add docstrings and alter try except statement

--- create_database.py
+++ create_database.py
@@ -1,19 +1,35 @@
 import sys
 import sqlite3
-import pandas as pd
 
 
-def connect_to_sqlite3(dbFile, dataframe):
+def connect_to_sqlite3(db_file, dataframe):
+    """
+    Connects to a SQLite3 database and creates a table.
+
+    Parameters:
+    db_file (str): The database file to connect to.
+    dataframe (pandas.DataFrame): The dataframe to convert into a SQL table.
+
+    Raises:
+    sqlite3.Error: If a connection error occurs.
+    """
     conn = None
     try:
-        conn = sqlite3.connect(dbFile)
-        create_database(dataframe=dataframe, connection=conn)
+        conn = sqlite3.connect(db_file)
+        create_table(dataframe=dataframe, connection=conn)
     except sqlite3.Error as e:
         print(f"Failed to create a connection to sqlite3: {e}")
-        sys.exit(1)
+        raise
     finally:
         conn.close()
 
 
-def create_database(dataframe, connection):
+def create_table(dataframe, connection):
+    """
+    Creates a table in the SQLite3 database from a pandas DataFrame.
+
+    Parameters:
+    dataframe (pandas.DataFrame): The dataframe to convert into a SQL table.
+    connection (sqlite3.Connection): The connection to the SQLite3 database.
+    """
     dataframe.to_sql('family_tree', connection, if_exists='replace')
