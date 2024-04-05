1bd50029f05fa0ce9f2e2c802ca74f48f204e216
mal-nushi-dev <malnushi1999+dev@gmail.com>
2024-04-05 15:32:59 -0400

    Refactor streamlit_app.py for improved code organization and readability

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,12 @@
 import configparser
 import sqlite3
-import streamlit_app as st
+import streamlit as st
 import pandas as pd
 
-# ------------------------ CONFIGURATIONS ------------------------
-page_title = "NUSHI GENEALOGY"
-layout = "centered"
-# ----------------------------------------------------------------
-
+# Set page configurations
 st.set_page_config(
-    page_title=page_title,
-    layout=layout
+    layout="centered"
 )
-
-# Print page title
-st.title(page_title)
 
 
 class DatabasePage:
@@ -22,21 +14,34 @@ class DatabasePage:
         self.database_path = database_path
         self.table_name = table_name
 
-    def load_data(self, table_name: str) -> pd.DataFrame:
+    # Load data from the database
+    def load_data(self) -> pd.DataFrame:
         conn = sqlite3.connect(self.database_path)
         query = f"SELECT * FROM {self.table_name};"
         db_page_df = pd.read_sql_query(sql=query, con=conn)
         return db_page_df
-    
+
+    # Display the data on the Streamlit page
     def run(self):
-        db_page_df = self.load_data(self.table_name)
+        db_page_df = self.load_data()
         st.dataframe(db_page_df)
 
 
 def main():
+    # Read configurations from config.ini
     config = configparser.ConfigParser()
     config.read('config.ini')
     family_echo_config = config['family_echo']
+    table_name = family_echo_config['family_name'].upper()
+
+    # Print page title
+    st.title(f"{table_name} GENEALOGY")
+
+    # Create a DatabasePage object and run it
     database_page = DatabasePage(database_path='family_trees.db',
-                                 table_name=family_echo_config['family_name'])
+                                 table_name=table_name)
     database_page.run()
+
+
+if __name__ == "__main__":
+    main()
