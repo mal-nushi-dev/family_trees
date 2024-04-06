b21d593c2ee2229c9b162b1149115852090b9c9f
Mal Nushi <malnushi1999+dev@gmail.com>
2024-04-05 23:29:57 -0400

    Simplified homepage streamlit code

--- streamlit/01_Homepage.py
+++ streamlit/01_Homepage.py
@@ -1,8 +1,8 @@
 from scripts.streamlit_filter_dataframe import filtered_df
-import configparser
 import sqlite3
 import streamlit as st
 import pandas as pd
+
 
 # Set page configurations
 st.set_page_config(
@@ -10,39 +10,20 @@ st.set_page_config(
 )
 
 
-class DatabasePage:
-    def __init__(self, database_path: str, table_name: str) -> None:
-        self.database_path = database_path
-        self.table_name = table_name
-
-    # Load data from the database
-    def load_data(self) -> pd.DataFrame:
-        conn = sqlite3.connect(self.database_path)
-        query = f"SELECT * FROM {self.table_name};"
-        db_page_df = pd.read_sql_query(sql=query, con=conn)
-        df_filter = filtered_df(db_page_df)
-        db = df_filter.filter_dataframe()
-        return db
-
-    # Display the data on the Streamlit page
-    def run(self):
-        db_page_df = self.load_data()
-        st.dataframe(db_page_df)
+# Load data from the database
+def create_dataframe(database_path, table_name):
+    conn = sqlite3.connect(database_path)
+    query = f"SELECT * FROM {table_name};"
+    database_df = pd.read_sql_query(sql=query, con=conn)
+    df_filter = filtered_df(database_df)
+    database = df_filter.filter_dataframe()
+    return database
 
 
-def main():
-    # Read configurations from config.ini
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+# Print page title
+st.title("NUSHI GENEALOGY")
 
-    # Print page title
-    st.title("NUSHI GENEALOGY")
-
-    # Create a DatabasePage object and run it
-    database_page = DatabasePage(database_path='family_trees.db',
+# Create a DatabasePage object and run it
+database_page = create_dataframe(database_path='family_trees.db',
                                  table_name="NUSHI")
-    database_page.run()
-
-
-if __name__ == "__main__":
-    main()
+st.dataframe(database_page)
